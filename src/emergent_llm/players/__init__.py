17502fc825c877fc5a882d2b6b8db3936862c627
Muff2n <richard.willis5@gmail.com>
2025-07-22 18:19:14 +0100

    tests in progress

--- src/emergent_llm/players/__init__.py
+++ src/emergent_llm/players/__init__.py
@@ -1,5 +1,5 @@
 """Player classes for social dilemma experiments."""
-from .base_player import BasePlayer
+from .base_player import BasePlayer, BaseStrategy
 from .players import LLMPlayer, SimplePlayer
 
 __all__ = ['BasePlayer', 'LLMPlayer', 'SimplePlayer']
